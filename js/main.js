19a40376ac15a45c73723e334a7cad0f0175ac16
Jonathan Lee <Jonwlee457@gmail.com>
2015-08-21 00:22:35 -0400

    adding functionality
    writing main function in main.js
    minor ui changes

--- js/main.js
+++ js/main.js
@@ -20,3 +20,26 @@ function disableOther( button ) {
 	}
 
 }
+
+
+function textToArray(){
+	var input;
+  var stringArray = [];    
+  var vocabList = document.getElementById('input').value
+var stringArray = document.getElementById('input').value.split("\n");
+var length = stringArray.length;
+if (length > 0){
+	var i = 0
+}
+else {
+	alert("Nothing to be Defined");
+} 
+
+for (i = 0; var input = stringArray[i] ; i++) { 
+    var url = "http://api.wolframalpha.com/v2/query?appid=J683VL-3XY28ALK48&input=define%20" + input + "&format=plaintext";
+
+   	send to to wolfram 
+
+
+   }
+
