4b2342410dfb486f03fe635e201044f9f927dd1a
Jonathan Lee <Jonwlee457@gmail.com>
2015-08-21 01:11:48 -0400

    i think  it works
    cant run from a local file?
    need to host a webserver, but simple html python server didnt work

--- js/main.js
+++ js/main.js
@@ -1,3 +1,9 @@
+document.addEventListener('DOMContentLoaded', //this is one big parameter
+    function handlers() {
+    document.getElementById('defined').addEventListener('click', textToArray);
+
+});
+
 var 
 		menuRight = document.getElementById( 'cbp-spmenu-s2' ),
 		
@@ -22,8 +28,48 @@ function disableOther( button ) {
 }
 
 
+
+function loadXMLDoc(url) {
+   
+xmlhttp=null;
+if (window.XMLHttpRequest)
+  {
+      xmlhttp=new XMLHttpRequest();
+  }
+if (xmlhttp!=null)
+  {
+      xmlhttp.onreadystatechange=state_Change;
+      xmlhttp.open("GET",url,true);
+      xmlhttp.send(null);
+  }
+}
+
+
+
+function state_Change() {
+    if (xmlhttp.readyState==4)
+      {// 4 = "loaded"
+          if (xmlhttp.status==200)
+            {// 200 = OK
+             //xmlhttp.data and shtuff
+            // ...our code here...
+            var definition = xmlDoc.getElementsByTagName("plaintext")[0];
+            document.getElementById('output').value = definition
+
+
+        }
+    }
+  else
+        {
+            alert("Problem retrieving data");
+        }
+  }
+
+
 function textToArray(){
 	var input;
+	var url;
+	   	var xmlhttp; //globals, needed for xml
   var stringArray = [];    
   var vocabList = document.getElementById('input').value
 var stringArray = document.getElementById('input').value.split("\n");
@@ -32,14 +78,20 @@ if (length > 0){
 	var i = 0
 }
 else {
-	alert("Nothing to be Defined");
-} 
-
-for (i = 0; var input = stringArray[i] ; i++) { 
-    var url = "http://api.wolframalpha.com/v2/query?appid=J683VL-3XY28ALK48&input=define%20" + input + "&format=plaintext";
-
-   	send to to wolfram 
+	alert("Nothing to be Defined"); } 
 
 
-   }
+
+for (i = 0; input = stringArray[i] ; i++) { 
+    url = "http://api.wolframalpha.com/v2/query?appid=J683VL-3XY28ALK48&input=define%20" + input + "&format=plaintext";
+
+
+
+   	loadXMLDoc(url) 
+
+
+}
+}
+
+
 
